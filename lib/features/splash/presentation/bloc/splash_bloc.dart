1b10bb4aa571b77d3971b10d37d19ec800dcecc9
vinihzets <viniciusdaarky@outlook.com>
2023-04-29 17:23:18 -0300

    implementado verificacao de authenticacao

--- lib/features/splash/presentation/bloc/splash_bloc.dart
+++ lib/features/splash/presentation/bloc/splash_bloc.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_3/core/architeture/bloc.dart';
 import 'package:pokedex_3/core/architeture/event.dart';
+import 'package:pokedex_3/core/services/auth/auth_service.dart';
 import 'package:pokedex_3/core/utils/consts.dart';
 import 'package:pokedex_3/features/splash/presentation/bloc/splash_event.dart';
 
 class SplashBloc extends Bloc {
   ConstsRoutes routes;
+  AuthService auth;
 
-  SplashBloc(this.routes) {}
+  SplashBloc(this.routes, this.auth) {}
 
   @override
   mapListenEvent(Event event) {
@@ -17,7 +19,12 @@ class SplashBloc extends Bloc {
   }
 
   _handleAuthentication(BuildContext context) {
-    Future.delayed(const Duration(seconds: 3))
-        .then((_) => navigateRemoveUntil(context, routes.loginView));
+    Future.delayed(const Duration(seconds: 3)).then((_) {
+      if (auth.auth.currentUser != null) {
+        return navigateRemoveUntil(context, routes.homeView);
+      } else if (auth.auth.currentUser == null) {
+        return navigateRemoveUntil(context, routes.loginView);
+      }
+    });
   }
 }
