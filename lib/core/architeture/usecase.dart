81605063f204c6f0a6f580f5a3ba444dedd83ca0
vinihzets <viniciusdaarky@outlook.com>
2023-05-03 14:30:28 -0300

    <-- implementando params pro UseCase

--- lib/core/architeture/usecase.dart
+++ lib/core/architeture/usecase.dart
@@ -4,3 +4,18 @@ import 'package:pokedex_3/core/failure/failure.dart';
 abstract class UseCase<Input, Output> {
   Future<Either<Failure, Output>> call(Input params);
 }
+
+class NoParams {}
+
+class IndexApiParams {
+  int currentIndex;
+  int limit;
+
+  IndexApiParams(this.currentIndex, this.limit);
+}
+
+class TypeParams {
+  String url;
+
+  TypeParams(this.url);
+}
