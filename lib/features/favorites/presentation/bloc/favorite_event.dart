bfa5b8d3edf15a963943a1f9eb16210f432f4b3a
vinihzets <viniciusdaarky@outlook.com>
2023-05-04 17:31:26 -0300

    implementado evento de navegacao

--- lib/features/favorites/presentation/bloc/favorite_event.dart
+++ lib/features/favorites/presentation/bloc/favorite_event.dart
@@ -1,5 +1,13 @@
+import 'package:flutter/widgets.dart';
 import 'package:pokedex_3/core/architeture/event.dart';
 
 abstract class FavoriteEvent extends Event {}
 
 class FavoriteEventGetListFavorites implements FavoriteEvent {}
+
+class FavoriteEventNavigateToDetails implements FavoriteEvent {
+  BuildContext context;
+  Object args;
+
+  FavoriteEventNavigateToDetails(this.context, this.args);
+}
