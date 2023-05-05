fc925d5dbabbae38e20c867bfd3137c0ac4c07b1
vinihzets <viniciusdaarky@outlook.com>
2023-05-05 11:33:13 -0300

    implementado ao se registrar usuario ja ganha alguns items iniciais

--- lib/features/register/data/datasources/remote/register_datasources_remote_imp.dart
+++ lib/features/register/data/datasources/remote/register_datasources_remote_imp.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:pokedex_3/core/global/mappers/pokeball_mapper.dart';
 import 'package:pokedex_3/core/services/auth/auth_service.dart';
 import 'package:pokedex_3/core/services/database/database_service.dart';
 import 'package:pokedex_3/features/register/data/datasources/mappers/register_mapper.dart';
@@ -18,6 +19,16 @@ class RegisterDataSourcesRemoteImpl implements RegisterDataSources {
     final auth = await authService.auth.createUserWithEmailAndPassword(
         email: params.email, password: params.password);
     final db = databaseService.database.collection('users').add(mapper.toMap());
+
+    PokeballMapper pokeball = PokeballMapper('pokeball', 10, 0.2);
+
+    final inventoryAdd = await databaseService.database
+        .collection('inventory')
+        .add(pokeball.toMap())
+        .then((value) => databaseService.database
+            .collection('inventory')
+            .doc(value.id)
+            .update({'docId': value.id, 'userId': auth.user!.uid}));
 
     return auth;
   }
