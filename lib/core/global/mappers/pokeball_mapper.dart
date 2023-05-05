c8447d0bfc964ba50ccdd0e6f347419da4836116
vinihzets <viniciusdaarky@outlook.com>
2023-05-05 13:16:39 -0300

    pequenas alteracoes

--- lib/core/global/mappers/pokeball_mapper.dart
+++ lib/core/global/mappers/pokeball_mapper.dart
@@ -3,13 +3,23 @@ import 'package:pokedex_3/core/global/entities/pokeball_entity.dart';
 class PokeballMapper extends PokeballEntity {
   PokeballMapper(super.ball, super.quantity, super.rate);
 
+  factory PokeballMapper.fromMap(dynamic map) {
+    return PokeballMapper(
+      map['ball'] ?? '',
+      map['quantity'] ?? 0,
+      map['rate'] ?? 0.0,
+    );
+  }
+
   toMap() {
     return {
-      'pokeballs': {
-        'ball': ball,
-        'rate': rate,
-        'quantity': quantity,
-      }
+      'pokeballs': [
+        {
+          'ball': ball,
+          'rate': rate,
+          'quantity': quantity,
+        }
+      ]
     };
   }
 }
--- lib/features/inventory/data/mappers/inventory_mapper.dart
+++ lib/features/inventory/data/mappers/inventory_mapper.dart
@@ -1,3 +1,4 @@
+import 'package:pokedex_3/core/global/mappers/pokeball_mapper.dart';
 import 'package:pokedex_3/features/inventory/domain/entities/inventory_entity.dart';
 
 class InventoryMapper extends InventoryEntity {
@@ -5,7 +6,7 @@ class InventoryMapper extends InventoryEntity {
 
   factory InventoryMapper.fromMap(dynamic map) {
     return InventoryMapper(
-      map['pokeballs'] ?? [],
+      (map['pokeballs'] as List).map((e) => PokeballMapper.fromMap(e)).toList(),
       map['pokemons'] ?? [],
       map['userId'] ?? '',
     );
