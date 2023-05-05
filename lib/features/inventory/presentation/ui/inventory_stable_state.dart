5fdcecdb30573c24ae8e9dc8ddff1952f58e50d8
vinihzets <viniciusdaarky@outlook.com>
2023-05-05 13:16:27 -0300

    implementado widgets da invetory feature

--- lib/features/inventory/presentation/ui/inventory_stable_state.dart
+++ lib/features/inventory/presentation/ui/inventory_stable_state.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:pokedex_3/core/architeture/bloc_state.dart';
+import 'package:pokedex_3/features/inventory/domain/entities/inventory_entity.dart';
 
 class InventoryStableState extends StatelessWidget {
   final BlocState state;
@@ -7,6 +10,45 @@ class InventoryStableState extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final List<InventoryEntity> listInventory = state.data;
+
+    return Scaffold(
+        body: Column(
+      children: [
+        Card(
+          child: ExpansionTile(
+            title: const Text('items'),
+            children: listInventory
+                .map((e) => Column(
+                      children: e.pokeballs.isNotEmpty
+                          ? e.pokeballs
+                              .map((e) => ListTile(
+                                    leading: const CircleAvatar(
+                                      backgroundImage: NetworkImage(
+                                          'https://www.pokemoncenter.com/images/DAMRoot/Full-Size/10000/P8737_710-95848_02.jpg'),
+                                    ),
+                                    title: Text(e.ball),
+                                    trailing: Text(e.quantity.toString()),
+                                  ))
+                              .toList()
+                          : [const Text('Nenhuma pokeball adquirida ainda')],
+                    ))
+                .toList(),
+          ),
+        ),
+        Card(
+          child: ExpansionTile(
+            title: const Text('Pokemons'),
+            children: listInventory
+                .map((e) => Column(
+                      children: e.pokemons.isNotEmpty
+                          ? e.pokemons.map((e) => Text(e.name)).toList()
+                          : [const Text('Nenhum pokemon capturado ainda ')],
+                    ))
+                .toList(),
+          ),
+        )
+      ],
+    ));
   }
 }
